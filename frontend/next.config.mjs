c9a39ce842b07cc19b12b9cc6ef6d19e0f3483cf
mezhack <ecomptjoaopedro@gmail.com>
2026-04-25 17:32:07 -0300

    retirado modo standalone

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -15,8 +15,6 @@ const nextConfig = {
       { protocol: "https", hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com" },
     ],
   },
-  // Útil para desacoplar da URL pública nas builds.
-  output: "standalone",
 }
 
 export default nextConfig
